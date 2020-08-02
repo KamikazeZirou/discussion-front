9e8764b9c3ce958c0576ed22fcbcdacbf2311667
kamikaze.zirou <kamikaze.zirou@gmail.com>
2020-08-03 01:29:23 +0900

    chore: Reactで記述

---
+++ src/main/kotlin/App.kt
@@ -0,0 +1,43 @@
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.await
+import kotlinx.coroutines.launch
+import org.w3c.fetch.CORS
+import org.w3c.fetch.RequestInit
+import org.w3c.fetch.RequestMode
+import react.*
+import react.dom.div
+import kotlin.browser.window
+
+class App : RComponent<RProps, AppState>() {
+    override fun AppState.init() {
+        val mainScope = MainScope()
+        mainScope.launch {
+            val status = checkHealth()
+            setState {
+                this.status = status
+            }
+        }
+    }
+
+    override fun RBuilder.render() {
+        div {
+            +(state.status ?: "Checking...")
+        }
+    }
+
+    private suspend fun checkHealth(): String {
+        return window
+            .fetch(
+                "http://localhost:8080/health_check",
+                RequestInit(mode = RequestMode.CORS)
+            )
+            .await()
+            .text()
+            .await()
+    }
+}
+
+external interface AppState : RState {
+    var status: String?
+}
+
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,24 +1,8 @@
-import kotlinx.coroutines.await
-import org.w3c.fetch.CORS
-import org.w3c.fetch.RequestInit
-import org.w3c.fetch.RequestMode
+import react.dom.render
 import kotlin.browser.document
-import kotlin.browser.window
 
-suspend fun main() {
-    document.write("Checking ...")
-    checkHealth()
-}
-
-suspend fun checkHealth() {
-    val result = window
-        .fetch(
-            "http://localhost:8080/health_check",
-            RequestInit(mode = RequestMode.CORS)
-        )
-        .await()
-        .text()
-        .await()
-
-    document.write(result)
+fun main() {
+    render(document.getElementById("root")) {
+        child(App::class) {}
+    }
 }
