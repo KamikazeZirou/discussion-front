d591f45938e1c68bf6b50b36996a3d58e84efb20
kamikaze.zirou <kamikaze.zirou@gmail.com>
2020-08-03 00:46:43 +0900

    feat: バックエンドと通信する

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,24 @@
+import kotlinx.coroutines.await
+import org.w3c.fetch.CORS
+import org.w3c.fetch.RequestInit
+import org.w3c.fetch.RequestMode
 import kotlin.browser.document
+import kotlin.browser.window
 
-fun main() {
-    document.write("Hello, world!")
+suspend fun main() {
+    document.write("Checking ...")
+    checkHealth()
+}
+
+suspend fun checkHealth() {
+    val result = window
+        .fetch(
+            "http://localhost:8080/health_check",
+            RequestInit(mode = RequestMode.CORS)
+        )
+        .await()
+        .text()
+        .await()
+
+    document.write(result)
 }
